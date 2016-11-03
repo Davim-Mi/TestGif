a0d239907ea99416e21c8bfc6340d254bad08cde
Davim <mizhongqin@iqiyi.pps>
2016-11-03 20:14:57 +0800

    Initial commit

---
+++ app/src/main/java/com/qiyi/testgif/MainActivity.java
@@ -0,0 +1,18 @@
+package com.qiyi.testgif;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ImageView imageView = (ImageView)findViewById(R.id.imv_test_gif);
+//        Glide.with(this).load(R.drawable.test_500_500).into(imageView);
+
+    }
+
+
+}
